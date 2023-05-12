d9ce208bb601793714dcc8afb6e66d98bec3b79f
metaproph3t <metaproph3t@protonmail.com>
2023-05-12 00:00:00 +0000

    feat: start `Front` and `Back` implementation

--- programs/basic-token-backend/src/lib.rs
+++ programs/basic-token-backend/src/lib.rs
@@ -1,36 +1,61 @@
 use anchor_lang::prelude::*;
+use modular_token::TokenAccountFront;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[derive(Accounts)]
-pub struct InitializeMint {}
+pub struct InitializeMint<'info> {
+    #[account(init, payer = payer, space = 8 + 32 + 8 + 1)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[derive(Accounts)]
-pub struct InitializeTokenAccount {}
+#[instruction(mint: u64)]
+pub struct InitializeTokenAccount<'info> {
+    pub token_account_back: Account<'info, TokenAccountBack>,
+    pub token_account_front: Account<'info, TokenAccountFront>,
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
-#[derive(Accounts)]
-pub struct MintTo {}
-
-#[derive(Accounts)]
-pub struct Transfer {}
 
 #[program]
 pub mod basic_token_backend {
     use super::*;
 
-    pub fn initialize_mint(ctx: Context<InitializeMint>) -> Result<()> {
+    pub fn initialize_token_account(
+        _ctx: Context<InitializeTokenAccount>,
+    ) -> Result<()> {
         Ok(())
     }
 
-    pub fn initialize_token_account(ctx: Context<InitializeTokenAccount>) -> Result<()> {
-        Ok(())
-    }
+    pub fn initialize_mint(
+        ctx: Context<InitializeMint>,
+        mint_authority: Pubkey,
+        decimals: u8,
+        // _data: Vec<u8>,
+    ) -> Result<()> {
+        let mint = &mut ctx.accounts.mint;
 
-    pub fn mint_to(ctx: Context<MintTo>) -> Result<()> {
-        Ok(())
-    }
+        mint.mint_authority = mint_authority;
+        mint.decimals = decimals;
 
-    pub fn transfer(ctx: Context<InitializeMint>) -> Result<()> {
         Ok(())
     }
+}
+
+#[account]
+pub struct Mint {
+    pub mint_authority: Pubkey,
+    pub supply: u64,
+    pub decimals: u8,
+}
+
+#[account]
+pub struct TokenAccountBack {
+    pub balance: u64,
 }
--- tests/modular-token.ts
+++ tests/modular-token.ts
@@ -1,16 +1,62 @@
 import * as anchor from "@coral-xyz/anchor";
+// import * as solana from "@solana/web3.js";
+
 import { Program } from "@coral-xyz/anchor";
 import { ModularToken } from "../target/types/modular_token";
+import { BasicTokenBackend } from "../target/types/basic_token_backend";
 
 describe("modular-token", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.ModularToken as Program<ModularToken>;
+  const token = anchor.workspace.ModularToken as Program<ModularToken>;
+  const backend = anchor.workspace.BasicTokenBackend as Program<BasicTokenBackend>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Can initialize token accounts", async () => {
+    const tokenAccountFrontKP = anchor.web3.Keypair.generate();
+    const mint = new anchor.BN(0);
+
+    const [tokenAccountFront] = anchor.web3.PublicKey.findProgramAddressSync(
+      [
+        anchor.utils.bytes.utf8.encode("token_account_front"),
+        Buffer.from(mint.toArray("le")),
+        token.provider.publicKey.toBuffer()
+      ],
+      token.programId
+    );
+
+    console.log(tokenAccountFront);
+
+    // anchor.web3.PublicKey.findProgramAddressSync([
+    //   anchor.utils.bytes.utf8.encode("token_account_back"),
+
+    // ])
+
+    // anchor.web3.SystemProgram.createAccountWithSeed({
+    //   fromPubkey: token.provider.publicKey,
+    //   basePubkey: token.provider.publicKey,
+    //   seed: "TEST",
+    //   space: 300,
+    //   lamports: 100000,
+    //   programId: backend.programId,
+    // })
+
+
+    // await program.methods
+    //   .initializeTokenAccount(program.provider.publicKey, mint)
+    //   .accounts({
+    //     // backe
+    //     tokenAccountFront: tokenAccountFrontKP.publicKey,
+    //     initializer: program.provider.publicKey,
+    //     systemProgram: anchor.web3.SystemProgram.programId,
+    //   })
+    //   .signers([tokenAccountFrontKP])
+    //   .rpc();
+
+    // const storedTokenAccount = await program.account.tokenAccountFront.fetch(
+    //   tokenAccountFrontKP.publicKey
+    // );
+
+    // console.log(storedTokenAccount);
   });
 });
